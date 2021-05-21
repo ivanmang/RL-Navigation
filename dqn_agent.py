6b6ca6686894d26fc4de96d9c2f8852794df05dd
Ivan Mang <ivanmang1997@gmail.com>
2021-05-21 17:34:57 +0800

    Add Double DQN Agent which choose actions based on local network

--- dqn_agent.py
+++ dqn_agent.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
+BATCH_SIZE = 32         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate 
@@ -85,8 +85,9 @@ class Agent():
         """
         states, actions, rewards, next_states, dones = experiences
 
+        local_argmax_actions = self.qnetwork_local(next_states).detach().argmax(dim=1).unsqueeze(1)
         # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        Q_targets_next = self.qnetwork_target(next_states).gather(1,local_argmax_actions).detach()
         # Compute Q targets for current states 
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
